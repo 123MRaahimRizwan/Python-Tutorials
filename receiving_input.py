fb180739fcfdc720439cbd69a3be2f52cfb0c194
M.Raahim Rizwan <raahimrizwantcs@gmail.com>
2021-10-26 18:22:26 -0700

    Video 08 Completed

---
+++ receiving_input.py
@@ -0,0 +1,13 @@
+# Receiving Input in python
+
+# print("Enter a number")
+# input()
+# print("Enter a second number")
+# input()
+
+# Calculator in Python
+print("Enter a number")
+n1 = input()
+print("Enter a second number")
+n2 = input()
+print("The sum of these two numbers is ", int(n1) + int(n2))
