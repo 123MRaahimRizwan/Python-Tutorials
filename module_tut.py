c4e68800f4b7bdc863647ad142ba3fc4ccc569dc
M.Raahim Rizwan <raahimrizwantcs@gmail.com>
2021-11-03 12:59:26 -0700

    Video 11 Completed

---
+++ module_tut.py
@@ -0,0 +1,14 @@
+# Modules are the python code written by other python developers in order to make our life much easier.
+
+# 1. This is the first method to import a module
+import matplotlib.pyplot as plt  # used for making graphs
+from tkinter import *
+import random
+
+print(random.randint(1, 5))
+
+# 2. Second Method
+root = Tk()
+root.mainloop()
+
+# 3. Third Method
